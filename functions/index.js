a85a99c27d8ebdd6d2c25ce45a744f6227490053
Kevin <kevincrisanto@gmail.com>
2018-05-30 21:51:43 -0700

    Created Cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,48 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+exports.onLikeDog = functions.firestore
+    .document('/likes/{likeId}')
+    .onCreate(event => {
+        let dogId, userId;
+        [dogId, userId] = event.params.likeId.split(':');
+
+        const db = admin.firestore();
+        const dogRef = db.collection('dogs').doc(dogId);
+        db.runTransaction(t => {
+            return t.get(dogRef)
+                .then(doc => {
+                    t.update(dogRef, {
+                        like_counter: (doc.data().like_counter || 0) + 1
+                    });
+                })
+        }).then(result => {
+            console.log('Increased aggregate dog like counter.');
+        }).catch(err => {
+            console.log('Failed to increase aggregate dog like counter.', err);
+        });
+    });
+
+exports.onUnlikeDog = functions.firestore
+    .document('/likes/{likeId}')
+    .onDelete(event => {
+        let dogId, userId;
+        [dogId, userId] = event.params.likeId.split(':');
+
+        const db = admin.firestore();
+        const dogRef = db.collection('dogs').doc(dogId);
+        return db.runTransaction(t => {
+            return t.get(dogRef)
+                .then(doc => {
+                    t.update(dogRef, {
+                        like_counter: (doc.data().like_counter || 0) - 1
+                    });
+                })
+        }).then(result => {
+            console.log('Decreased aggregate dog like counter.');
+        }).catch(err => {
+            console.log('Failed to decrease aggregate dog like counter.', err);
+        });
+    });
